2da269197f006be3bce02ab346b584fc9f9b3c11
rob-sanz <robertosanchez@unah.hn>
2025-04-22 22:34:05 -0600

    Add EmpleadoView for employee management; implement UI and initial data loading structure

---
+++ Views/EmpleadoView.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ProyectoBD2.Views;
+
+public partial class EmpleadoView : UserControl
+{
+    public EmpleadoView()
+    {
+        InitializeComponent();
+        
+        
+    }
+    
+    
+    public void loadEmpleado()
+    {
+        // Aquí puedes cargar los datos del empleado
+        // Por ejemplo, puedes usar un servicio para obtener los datos y luego asignarlos a las propiedades de la vista
+        
+        
+    }
+}
