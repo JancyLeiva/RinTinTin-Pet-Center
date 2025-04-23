3720dd541da4eef141348170512cfd04adb796d4
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2025-04-23 02:06:09 -0600

    Add PurchasesView for managing purchases; implement purchase history retrieval and creation functionality

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -4,7 +4,9 @@ namespace ProyectoBD2.Models;
 
 public class Purchase
 {
-    public int? ProveedorId { get; set; }
+    public int? CompraId { get; set; }
+    public string? CodigoCompra { get; set; }
+    public string? Proveedor { get; set; }
     public DateTime? Fecha { get; set; }
-    public string? Estado { get; set; }
+    public decimal? Total { get; set; }
 }
